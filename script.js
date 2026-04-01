dc0bb3c0d4130c00755c05ba7d689ccc7f01d7d7
Sagarmogli8976 <114072291+Sagarmogli8976@users.noreply.github.com>
2026-04-01 23:14:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function firstWord(s) {
   // your code here
+	 if (!s) return "";
+
+  s = s.trimStart();
+
+  let index = s.indexOf(" ");
+
+  if (index === -1) {
+    return s;
+  }
+
+  // return substring till first space
+  return s.slice(0, index);
 }
 
 // Do not change the code below
